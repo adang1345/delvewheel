36ef4df63c961744e06410ad3a6f9dd905832a2a
Aohan Dang <adang1345@gmail.com>
2021-08-05 21:08:55 -0400

    Work around os.add_dll_directory bug for Anaconda
    For Anaconda distributions of Python, os.add_dll_directory does not work
    properly (https://github.com/ContinuumIO/anaconda-issues/issues/12475).
    A workaround is to set CONDA_DLL_SEARCH_MODIFICATION_ENABLE=1.
    
    Fix #14

--- delvewheel/wheel_repair.py
+++ delvewheel/wheel_repair.py
@@ -36,6 +36,8 @@ def _delvewheel_init_patch_{0}():
     import sys
     libs_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), os.pardir, {1!r}))
     if sys.version_info[:2] >= (3, 8):
+        if os.path.exists(os.path.join(sys.base_prefix, 'conda-meta')):
+            os.environ['CONDA_DLL_SEARCH_MODIFICATION_ENABLE']='1'
         os.add_dll_directory(libs_dir)
     else:
         from ctypes import WinDLL
