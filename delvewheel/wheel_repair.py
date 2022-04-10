e821f99a63bab5b7b5711d7e60c0790ed23d2f8b
Aohan Dang <adang1345@gmail.com>
2022-04-10 00:25:47 -0400

    Disable Anaconda workaround for Python 3.8.13
    3.8.13 is the first version in the 3.8 branch of Anaconda where the os.add_dll_directory() bug is fixed

--- delvewheel/wheel_repair.py
+++ delvewheel/wheel_repair.py
@@ -40,7 +40,7 @@ def _delvewheel_init_patch_{1}():
     import sys
     libs_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), os.pardir, {2!r}))
     if sys.version_info[:2] >= (3, 8):
-        conda_workaround = sys.version_info[:3] < (3, 9, 9) and os.path.exists(os.path.join(sys.base_prefix, 'conda-meta'))
+        conda_workaround = os.path.exists(os.path.join(sys.base_prefix, 'conda-meta')) and (sys.version_info[:3] < (3, 8, 13) or (3, 9, 0) <= sys.version_info[:3] < (3, 9, 9))
         if conda_workaround:
             # backup the state of the environment variable CONDA_DLL_SEARCH_MODIFICATION_ENABLE
             conda_dll_search_modification_enable = os.environ.get('CONDA_DLL_SEARCH_MODIFICATION_ENABLE')
