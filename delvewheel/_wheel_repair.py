419a2fe77422ca81f350bf1a01e11e66aad59493
Aohan Dang <adang1345@gmail.com>
2022-10-28 13:46:23 -0400

    Don't fail fast in __init__.py patch for PyInstaller environment
    When PyInstaller is used, files and folders expected in the __init__.py
    patch might not exist. To avoid raising an error immediately, we first
    check for existence of files and folders before attempting to access
    them if we are in a PyInstaller environment.
    
    In such a PyInstaller environment, it is possible that a DLL load error
    will happen later. However, we avoid raising an error if PyInstaller
    placed those DLLs in a different location in the DLL search path or if
    no dependent extension module is imported. Ultimately, it will be
    PyInstaller's responsibility to ensure that dependent DLLs are found at
    runtime.
    
    See https://github.com/pyinstaller/pyinstaller-hooks-contrib/issues/421
    for more details.

--- delvewheel/_wheel_repair.py
+++ delvewheel/_wheel_repair.py
@@ -41,14 +41,20 @@ def _delvewheel_init_patch_{1}():
     import os
     import sys
     libs_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), os.pardir, {2!r}))
+    is_pyinstaller = getattr(sys, 'frozen', False) and hasattr(sys, '_MEIPASS')
     if sys.version_info[:2] >= (3, 8) and not os.path.exists(os.path.join(sys.base_prefix, 'conda-meta')) or sys.version_info[:2] >= (3, 10):
-        os.add_dll_directory(libs_dir)
+        if not is_pyinstaller or os.path.isdir(libs_dir):
+            os.add_dll_directory(libs_dir)
     else:
         from ctypes import WinDLL
-        with open(os.path.join(libs_dir, {3!r})) as file:
-            load_order = file.read().split()
-        for lib in load_order:
-            WinDLL(os.path.join(libs_dir, lib))
+        load_order_filepath = os.path.join(libs_dir, {3!r})
+        if not is_pyinstaller or os.path.isfile(load_order_filepath):
+            with open(os.path.join(libs_dir, {3!r})) as file:
+                load_order = file.read().split()
+            for lib in load_order:
+                lib_path = os.path.join(os.path.join(libs_dir, lib))
+                if not is_pyinstaller or os.path.isfile(lib_path):
+                    WinDLL(lib_path)
 
 
 _delvewheel_init_patch_{1}()
@@ -69,8 +75,11 @@ _patch_init_template_v2 = """
 {0}# start delvewheel patch
 def _delvewheel_init_patch_{1}():
     import os
+    import sys
     libs_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), os.pardir, {2!r}))
-    os.add_dll_directory(libs_dir)
+    is_pyinstaller = getattr(sys, 'frozen', False) and hasattr(sys, '_MEIPASS')
+    if not is_pyinstaller or os.path.isdir(libs_dir):
+        os.add_dll_directory(libs_dir)
 
 
 _delvewheel_init_patch_{1}()
