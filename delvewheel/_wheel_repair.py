359eebc60880b615b4e5eaa28a541a172b7f37ab
Aohan Dang <adang1345@gmail.com>
2023-05-16 09:55:08 -0400

    Check for directory and file existence in __init__.py patch
    This improves compatibility with repackaging tools other than
    PyInstaller that change the location of the vendored DLLs.

--- delvewheel/_wheel_repair.py
+++ delvewheel/_wheel_repair.py
@@ -46,19 +46,18 @@ def _delvewheel_init_patch_{1}():
     import platform
     import sys
     libs_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), os.pardir, {2!r}))
-    is_pyinstaller = getattr(sys, 'frozen', False) and hasattr(sys, '_MEIPASS')
     is_conda_cpython = platform.python_implementation() == 'CPython' and (hasattr(ctypes.pythonapi, 'Anaconda_GetVersion') or 'packaged by conda-forge' in sys.version)
     if sys.version_info[:2] >= (3, 8) and not is_conda_cpython or sys.version_info[:2] >= (3, 10):
-        if not is_pyinstaller or os.path.isdir(libs_dir):
+        if os.path.isdir(libs_dir):
             os.add_dll_directory(libs_dir)
     else:
         load_order_filepath = os.path.join(libs_dir, {3!r})
-        if not is_pyinstaller or os.path.isfile(load_order_filepath):
+        if os.path.isfile(load_order_filepath):
             with open(os.path.join(libs_dir, {3!r})) as file:
                 load_order = file.read().split()
             for lib in load_order:
                 lib_path = os.path.join(os.path.join(libs_dir, lib))
-                if (not is_pyinstaller or os.path.isfile(lib_path)) and not ctypes.windll.kernel32.LoadLibraryExW(ctypes.c_wchar_p(lib_path), None, 0x00000008):
+                if os.path.isfile(lib_path) and not ctypes.windll.kernel32.LoadLibraryExW(ctypes.c_wchar_p(lib_path), None, 0x00000008):
                     raise OSError('Error loading {{}}; {{}}'.format(lib, ctypes.FormatError()))
 
 
@@ -85,8 +84,7 @@ def _delvewheel_init_patch_{1}():
     import os
     import sys
     libs_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), os.pardir, {2!r}))
-    is_pyinstaller = getattr(sys, 'frozen', False) and hasattr(sys, '_MEIPASS')
-    if not is_pyinstaller or os.path.isdir(libs_dir):
+    if os.path.isdir(libs_dir):
         os.add_dll_directory(libs_dir)
 
 
