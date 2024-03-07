adang1345/delvewheel

commit e1fd97ed9608e2ff838d5960c5ae995720499308

Support SOURCE_DATE_EPOCH for reproducible builds
Fix #41

delvewheel/_wheel_repair.py
@@ -3,6 +3,7 @@
 import ast
 import base64
 import csv
+import datetime
 import hashlib
 import os
 import pathlib
@@ -956,27 +957,43 @@ class WheelRepair:
         record_filepath = os.path.join(self._extract_dir, dist_info_foldername, 'RECORD')
         if self._verbose >= 1:
             print(f'updating {os.path.join(dist_info_foldername, "RECORD")}')
-        filepath_list = []
-        for root, _, files in os.walk(self._extract_dir):
-            for file in files:
-                filepath_list.append(os.path.join(root, file))
         with open(record_filepath, 'w', newline='\n') as record_file:
             writer = csv.writer(record_file, lineterminator='\n')
-            for file_path in filepath_list:
-                if file_path == record_filepath:
-                    writer.writerow((os.path.relpath(record_filepath, self._extract_dir).replace('\\', '/'), '', ''))
-                else:
-                    hash, size = self._rehash(file_path)
-                    writer.writerow((os.path.relpath(file_path, self._extract_dir).replace('\\', '/'), f'sha256={hash}', size))
+            for root, _, files in os.walk(self._extract_dir):
+                for file in files:
+                    file_path = os.path.join(root, file)
+                    if file_path == record_filepath:
+                        writer.writerow((os.path.relpath(record_filepath, self._extract_dir).replace('\\', '/'), '', ''))
+                    else:
+                        hash, size = self._rehash(file_path)
+                        writer.writerow((os.path.relpath(file_path, self._extract_dir).replace('\\', '/'), f'sha256={hash}', size))
 
         # repackage wheel
         print('repackaging wheel')
+        if 'SOURCE_DATE_EPOCH' in os.environ:
+            date_time = datetime.datetime.fromtimestamp(int(os.environ['SOURCE_DATE_EPOCH']), tz=datetime.timezone.utc).timetuple()[:6]
+        else:
+            date_time = None
         os.makedirs(target, exist_ok=True)
         whl_dest_path = os.path.join(target, self._whl_name)
         with zipfile.ZipFile(whl_dest_path, 'w', zipfile.ZIP_DEFLATED) as whl_file:
-            for file_path in filepath_list:
-                relpath = os.path.relpath(file_path, self._extract_dir)
-                if self._verbose >= 1:
-                    print(f'adding {relpath}')
-                whl_file.write(file_path, relpath)
+            for root, dirs, files in os.walk(self._extract_dir):
+                for dir in dirs:
+                    dir_path = os.path.join(root, dir)
+                    zip_dir_name = os.path.relpath(dir_path, self._extract_dir).replace('\\', '/') + '/'
+                    zip_info = zipfile.ZipInfo.from_file(dir_path, zip_dir_name)
+                    if date_time is not None:
+                        zip_info.date_time = date_time
+                    whl_file.writestr(zip_info, b'')
+                for file in files:
+                    file_path = os.path.join(root, file)
+                    relpath = os.path.relpath(file_path, self._extract_dir)
+                    zip_file_name = relpath.replace('\\', '/')
+                    zip_info = zipfile.ZipInfo.from_file(file_path, zip_file_name)
+                    if date_time is not None:
+                        zip_info.date_time = date_time
+                    if self._verbose >= 1:
+                        print(f'adding {relpath}')
+                    with open(file_path, 'rb') as f:
+                        whl_file.writestr(zip_info, f.read())
         print(f'fixed wheel written to {os.path.abspath(whl_dest_path)}')

tests/run_tests.py
@@ -1,3 +1,4 @@
+import collections.abc
 import os
 import re
 import shutil
@@ -11,10 +12,17 @@ import zipfile
 DEBUG = False
 
 
-def check_call(args: list):
+def check_call(args: list, env: typing.Optional[collections.abc.Mapping] = None):
+    base_env = os.environ.copy()
+    if env is not None:
+        for var in env:
+            if env[var] is None:
+                base_env.pop(var, None)
+            else:
+                base_env[var] = env[var]
     if DEBUG:
-        return subprocess.check_call(args)
-    return subprocess.check_call(args, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+        return subprocess.check_call(args, env=base_env)
+    return subprocess.check_call(args, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL, env=base_env)
 
 
 def is_mangled(filename: str) -> bool:
@@ -199,6 +207,7 @@ class ShowTestCase(unittest.TestCase):
         """Ignore .pyd file in .data/data directory."""
         output = subprocess.check_output(['delvewheel', 'show', 'simpleext/simpleext-0.0.1-0ignore-cp310-cp310-win_amd64.whl'], text=True)
         self.assertIn('will be copied into the wheel.\n    None', output)
+
 
 class RepairTestCase(TestCase):
     """Tests for delvewheel repair"""
@@ -1049,6 +1058,32 @@ class RepairTestCase(TestCase):
                 whl_file.extractall(tempdir)
             with open(os.path.join(tempdir, 'simpleext-0.0.1.dist-info/RECORD')) as file:
                 self.assertTrue(any(line.startswith('"simpleext-0.0.1.data/data/a,b.txt"') for line in file))
+
+    def test_source_date_epoch(self):
+        """The SOURCE_DATE_EPOCH environment variable can be used to have
+        reproducible builds."""
+        try:
+            check_call(['delvewheel', 'repair', '--add-path', 'simpleext/x64', '-w', 'wheelhouse1', 'simpleext/simpleext-0.0.1-cp310-cp310-win_amd64.whl'], {'SOURCE_DATE_EPOCH': None})
+            check_call(['delvewheel', 'repair', '--add-path', 'simpleext/x64', '-w', 'wheelhouse2', 'simpleext/simpleext-0.0.1-cp310-cp310-win_amd64.whl'], {'SOURCE_DATE_EPOCH': '650203200'})
+            check_call(['delvewheel', 'repair', '--add-path', 'simpleext/x64', '-w', 'wheelhouse3', 'simpleext/simpleext-0.0.1-cp310-cp310-win_amd64.whl'], {'SOURCE_DATE_EPOCH': '650203200'})
+            check_call(['delvewheel', 'repair', '--add-path', 'simpleext/x64', '-w', 'wheelhouse4', 'simpleext/simpleext-0.0.1-cp310-cp310-win_amd64.whl'], {'SOURCE_DATE_EPOCH': '650203202'})
+            with open('wheelhouse1/simpleext-0.0.1-cp310-cp310-win_amd64.whl', 'rb') as wheel1, \
+                open('wheelhouse2/simpleext-0.0.1-cp310-cp310-win_amd64.whl', 'rb') as wheel2, \
+                open('wheelhouse3/simpleext-0.0.1-cp310-cp310-win_amd64.whl', 'rb') as wheel3, \
+                open('wheelhouse4/simpleext-0.0.1-cp310-cp310-win_amd64.whl', 'rb') as wheel4:
+                contents1 = wheel1.read()
+                contents2 = wheel2.read()
+                contents3 = wheel3.read()
+                contents4 = wheel4.read()
+                self.assertNotEqual(contents1, contents2)
+                self.assertNotEqual(contents1, contents4)
+                self.assertEqual(contents2, contents3)
+                self.assertNotEqual(contents2, contents4)
+        finally:
+            remove('wheelhouse1/simpleext-0.0.1-cp310-cp310-win_amd64.whl')
+            remove('wheelhouse2/simpleext-0.0.1-cp310-cp310-win_amd64.whl')
+            remove('wheelhouse3/simpleext-0.0.1-cp310-cp310-win_amd64.whl')
+            remove('wheelhouse4/simpleext-0.0.1-cp310-cp310-win_amd64.whl')
 
 
 class NeededTestCase(unittest.TestCase):

delvewheel/__main__.py
@@ -81,7 +81,7 @@ def main():
             else:  # args.command == 'repair'
                 no_mangles = set(dll_name.lower() for dll_name in args.no_mangle.split(os.pathsep) if dll_name)
                 namespace_pkgs = set(tuple(namespace_pkg.split('.')) for namespace_pkg in args.namespace_pkg.split(os.pathsep) if namespace_pkg)
-                wr.repair(args.target, no_mangles, args.no_mangle_all, args.strip, args.lib_sdir, not args.no_diagnostic, namespace_pkgs, args.include_symbols)
+                wr.repair(args.target, no_mangles, args.no_mangle_all, args.strip, args.lib_sdir, not args.no_diagnostic and 'SOURCE_DATE_EPOCH' not in os.environ, namespace_pkgs, args.include_symbols)
     else:  # args.command == 'needed'
         for dll_name in sorted(_dll_utils.get_direct_needed(args.file, args.v), key=str.lower):
             print(dll_name)

commit b6a50a9f3d3236ae4df849b639c5e3c4b72de5d3

Fix regression where wheel contents were not compressed
Regression caused by e1fd97ed9608e2ff838d5960c5ae995720499308

delvewheel/_wheel_repair.py
@@ -984,6 +984,7 @@ class WheelRepair:
                     relpath = os.path.relpath(file_path, self._extract_dir)
                     zip_file_name = relpath.replace('\\', '/')
                     zip_info = zipfile.ZipInfo.from_file(file_path, zip_file_name)
+                    zip_info.compress_type = zipfile.ZIP_DEFLATED
                     if date_time is not None:
                         zip_info.date_time = date_time
                     if self._verbose >= 1:

tests/run_tests.py
@@ -505,8 +505,13 @@ class RepairTestCase(TestCase):
         self.assertTrue(import_iknowpy_successful('0normalpurelibplatlib', ['iknowpy', 'iknowpy2', 'iknowpy3']))
 
     def test_top_level(self):
-        """Top-level extension module in root directory"""
+        """Top-level extension module in root directory
+
+        Also check that the contents are compressed"""
         check_call(['delvewheel', 'repair', '--add-path', 'simpleext/x64', '--no-mangle-all', 'simpleext/simpleext-0.0.1-cp310-cp310-win_amd64.whl'])
+        with zipfile.ZipFile('wheelhouse/simpleext-0.0.1-cp310-cp310-win_amd64.whl') as whl:
+            zip_info = whl.getinfo('simpleext.cp310-win_amd64.pyd')
+            self.assertGreater(zip_info.file_size, zip_info.compress_size)
         self.assertTrue(import_simpleext_successful())
 
     def test_top_level_purelib(self):