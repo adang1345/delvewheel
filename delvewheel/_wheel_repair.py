2c01e20c9ba5121fe32f564eb7ee03324eed47b5
Aohan Dang <adang1345@gmail.com>
2024-08-08 10:20:13 -0400

    Improve performance when using .load-order file

--- delvewheel/_wheel_repair.py
+++ delvewheel/_wheel_repair.py
@@ -67,11 +67,11 @@ def _delvewheel_patch_{1}():
         if os.path.isfile(load_order_filepath):
             with open(os.path.join(libs_dir, {4!r})) as file:
                 load_order = file.read().split()
+            kernel32 = ctypes.WinDLL('kernel32', use_last_error=True)
+            kernel32.LoadLibraryExW.restype = ctypes.c_void_p
             for lib in load_order:
                 lib_path = os.path.join(os.path.join(libs_dir, lib))
-                kernel32 = ctypes.WinDLL('kernel32', use_last_error=True)
-                kernel32.LoadLibraryExW.restype = ctypes.c_void_p
-                if os.path.isfile(lib_path) and not kernel32.LoadLibraryExW(ctypes.c_wchar_p(lib_path), None, 0x00000008):
+                if os.path.isfile(lib_path) and not kernel32.LoadLibraryExW(ctypes.c_wchar_p(lib_path), None, 8):
                     raise OSError('Error loading {{}}; {{}}'.format(lib, ctypes.FormatError(ctypes.get_last_error())))
 
 
